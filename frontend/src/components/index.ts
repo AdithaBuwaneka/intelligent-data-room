15f07a9e64c79b53ced6480d909506a58944e47d
AdithaBuwaneka <adithabuwaneka0@gmail.com>
2026-01-27 02:03:37 +0530

    feat: add index file for exporting UI components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Component exports
+ */
+
+export { FileUpload } from './FileUpload';
+export { ChatInterface } from './ChatInterface';
+export { MessageList } from './MessageList';
+export { ChartDisplay } from './ChartDisplay';
+export { LoadingSpinner, TypingIndicator } from './LoadingSpinner';
